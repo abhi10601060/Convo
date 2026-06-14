0e7eb03ee880ca866e1bd2fc0743b8ba19be4689
Abhi <abhishekvelekar792000@gmail.com>
2026-06-14 20:31:43 +0530

    Add enhanced sms details dialog with scrollable body and reply functionality

--- feature/sms/src/main/java/com/app/sms/domain/model/SmsDomain.kt
+++ feature/sms/src/main/java/com/app/sms/domain/model/SmsDomain.kt
@@ -1,5 +1,7 @@
 package com.app.sms.domain.model
 
+import android.icu.util.Calendar
+
 data class SmsDomain(
     val id: String,
     val address: String, // Sender number
@@ -7,4 +9,15 @@ data class SmsDomain(
     val body: String,    // Message
     val date: Long,      // Timestamp
     val read: Boolean
-)
+){
+    companion object{
+        val dummy = SmsDomain(
+            id = "1",
+            address = "2345677890",
+            senderName = "Abhishek Velekar",
+            body = "This is a large Body",
+            date = Calendar.getInstance().time.time,
+            read = true
+        )
+    }
+}
--- feature/sms/src/main/java/com/app/sms/ui/component/SmsDetailDialog.kt
+++ feature/sms/src/main/java/com/app/sms/ui/component/SmsDetailDialog.kt
@@ -1,15 +1,23 @@
 package com.app.sms.ui.component
 
+import android.content.Intent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
 import com.app.sms.domain.model.SmsDomain
+import com.app.ui.theme.ConvoTheme
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -18,22 +26,44 @@ fun SmsDetailDialog(
     sms: SmsDomain,
     onDismiss: () -> Unit
 ) {
+    val context = LocalContext.current
+
     AlertDialog(
         onDismissRequest = onDismiss,
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(max = 650.dp),
         title = {
-            Text(
-                text = "From: ${sms.senderName ?: sms.address}",
-                style = MaterialTheme.typography.headlineSmall,
-                fontWeight = FontWeight.Bold
-            )
+            Column {
+                Text(
+                    text = "From: ${sms.senderName ?: sms.address}",
+                    style = MaterialTheme.typography.headlineSmall,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                HorizontalDivider(color = MaterialTheme.colorScheme.outlineVariant, thickness = 2.dp)
+            }
         },
         text = {
             Column {
-                Text(
-                    text = sms.body,
-                    style = MaterialTheme.typography.bodyLarge
+                Box(
+                    modifier = Modifier
+                        .weight(1f, fill = false)
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    Text(
+                        text = sms.body,
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.padding(vertical = 8.dp)
+                    )
+                }
+                
+                HorizontalDivider(
+                    modifier = Modifier.padding(vertical = 8.dp),
+                    color = MaterialTheme.colorScheme.outlineVariant,
+                    thickness = 2.dp
                 )
-                Spacer(modifier = Modifier.height(16.dp))
+
                 Text(
                     text = "Received: ${formatFullDate(sms.date)}",
                     style = MaterialTheme.typography.labelMedium,
@@ -45,6 +75,17 @@ fun SmsDetailDialog(
             TextButton(onClick = onDismiss) {
                 Text("Close")
             }
+        },
+        dismissButton = {
+            TextButton(
+                content = {
+                    Text("Reply")
+                },
+                onClick = {
+                    context.startActivity(Intent(Intent.ACTION_SENDTO, "smsto:${sms.address}".toUri()))
+                    onDismiss()
+                }
+            )
         }
     )
 }
@@ -53,4 +94,16 @@ private fun formatFullDate(timestamp: Long): String {
     val date = Date(timestamp)
     val sdf = SimpleDateFormat("dd MMM yyyy, HH:mm", Locale.getDefault())
     return sdf.format(date)
+}
+
+@Preview
+@Composable
+private fun DetailedSMSPrev() {
+    ConvoTheme {
+        SmsDetailDialog(
+            sms = SmsDomain.dummy
+        ){
+
+        }
+    }
 }
